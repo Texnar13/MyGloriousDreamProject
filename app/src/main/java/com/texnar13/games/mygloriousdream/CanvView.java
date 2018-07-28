50fdffd283e6884fda92dcb7e45b3ddebdf36e5a
Texnar13 <m-ivan-2000@mail.ru>
2018-07-28 13:20:49 +0300

    try

--- app/src/main/java/com/texnar13/games/mygloriousdream/CanvView.java
+++ app/src/main/java/com/texnar13/games/mygloriousdream/CanvView.java
@@ -2,19 +2,68 @@ package com.texnar13.games.mygloriousdream;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.CountDownTimer;
 import android.view.View;
 
 public class CanvView extends View {
 
-    //глаз
-    Bitmap eye;
+    //размеры view
+    int viewWidth;
+    int viewHeight;
 
+    //глаз
+    Sprite eyeSprite;
+
+    private final int timerInterval = 30;
 
     public CanvView(Context context) {
         super(context);
+        // --- инициализируем глаз ---
+        //получаем картинку
+        Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.eye_storm);
+        // задаем положение первого кадра и размеры кадров
+        int w = b.getWidth() / 2;
+        int h = b.getHeight() / 2;
+        Rect firstFrame = new Rect(0, 0, w, h);
+        // инициализируем
+        eyeSprite = new Sprite(10, 0, 10, 100, firstFrame, b);
+
+        //добавляем положение остальных кадров
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                if (i == 2 && j == 3) {
+                    continue;
+                }
+                eyeSprite.addFrame(new Rect(j * w, i * h, j * w + w, i * w + w));
+            }
+        }
+
+        //периодический таймер
+        Timer t = new Timer();
+        t.start();
+    }
+
+    // когда элементу присваивают новые размеры
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        viewWidth = w;
+        viewHeight = h;
+    }
+
+    //вывод гарафики
+    void update() {
+        //
+        eyeSprite.update(timerInterval);
+
+        invalidate();
     }
 
 
@@ -23,11 +72,32 @@ public class CanvView extends View {
     protected void onDraw(Canvas canvas) {
 
         super.onDraw(canvas);
+        super.onDraw(canvas);
+        // цвет фона
+        canvas.drawARGB(250, 127, 199, 255);
+        // отрисовка спрайта
+        eyeSprite.draw(canvas);
 
+        // текст
         Paint p = new Paint();
-        p.setColor(Color.BLUE);
-        p.setStyle(Paint.Style.FILL);
-        canvas.drawColor(Color.GRAY);
-        canvas.drawRect(100,100,200,200,p);
+        p.setAntiAlias(true);
+        p.setTextSize(55.0f);
+        p.setColor(Color.WHITE);
+        canvas.drawText(0 + "", viewWidth - 100, 70, p);
+    }
+
+    class Timer extends CountDownTimer {
+        public Timer() {
+            super(Integer.MAX_VALUE, timerInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            update();
+        }
+
+        @Override
+        public void onFinish() {
+        }
     }
 }
--- app/src/main/java/com/texnar13/games/mygloriousdream/Sprite.java
+++ app/src/main/java/com/texnar13/games/mygloriousdream/Sprite.java
@@ -1,5 +1,66 @@
 package com.texnar13.games.mygloriousdream;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sprite {
+
+    //глаз
+    Bitmap bitmap;
+    private List<Rect> frames;
+    private int frameWidth;
+    private int frameHeight;
+    private int currentFrame;
+    private double frameTime;
+    private double timeForCurrentFrame;
+    private double x;
+    private double y;
+    private double velocityX;
+    private double velocityY;
+    private int padding;
+
+
+    public Sprite(double x, double y, double velocityX, double velocityY, Rect initialFrame, Bitmap bitmap) {
+        this.x = x;
+        this.y = y;
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
+        this.bitmap = bitmap;
+        this.frames = new ArrayList<>();
+        this.frames.add(initialFrame);
+        this.timeForCurrentFrame = 0.0;
+        this.frameTime = 0.1;
+        this.currentFrame = 0;
+        this.frameWidth = initialFrame.width();
+        this.frameHeight = initialFrame.height();
+        this.padding = 20;
+    }
+
+    // метод добавления кадров
+    public void addFrame(Rect frame) {
+        frames.add(frame);
+    }
+
+    // обновление внутреннего состояния спрайта
+    public void update(int ms) {
+        timeForCurrentFrame += ms;
+        if (timeForCurrentFrame >= frameTime) {
+            currentFrame = (currentFrame + 1) % frames.size();
+            timeForCurrentFrame = timeForCurrentFrame - frameTime;
+        }
+        x = x + velocityX * ms / 1000.0;
+        y = y + velocityY * ms / 1000.0;
+    }
+
+    public void draw(Canvas canvas) {
+        Paint p = new Paint();
+        Rect destination = new Rect((int) x, (int) y, (int) (x + frameWidth), (int) (y + frameHeight));
+        canvas.drawBitmap(bitmap, frames.get(currentFrame), destination, p);
+    }
 
 }
